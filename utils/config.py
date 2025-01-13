62659a604cec007dbcfe7a5c4cd99af191a7853b
Arefeh Yavary <yavary.rf@gmail.com>
2025-01-13 08:38:10 -0800

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -1,1 +1,6 @@
+import os
+
+TOGETHER_API_KEY = os.getenv("TOGETHER_API_KEY")
+WANDB_API_KEY = os.getenv("WANDB_API_KEY")
+SYSTEM_PROMPT = "Read the story and extract answers for the questions.\nStory: {}"
 
